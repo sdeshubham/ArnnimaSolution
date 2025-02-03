a369d2096be31578df7cb6067034371c6cf4ffbf
shubham <skushwah106@gmail.com>
2025-02-03 10:42:27 +0530

    third path

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-};
-
-export default nextConfig;
+    images: {
+      unoptimized: true,
+    },
+  };
+  
+  export default nextConfig;
+  
